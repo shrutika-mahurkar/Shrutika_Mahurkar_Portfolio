fefec274a9810a7e0a2205427e9978f7927cd574
shrutika-mahurkar <shrutika.vmahurkar@gmail.com>
2021-12-11 18:13:17 -0800

    contact page

---
+++ contactConfirmation.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Thank You for reaching me out</h1>
+
+<?php
+
+?> 
+
+</body>
+</html>
--- javascripts/contact.js
+++ javascripts/contact.js
@@ -1,19 +1,19 @@
 
 
 
-form.addEventListener('submit',e=>{
-    e.preventDefault();
-    validateInputs();
-});
+// form.addEventListener('submit',e=>{
+//     e.preventDefault();
+//     validateInputs();
+// });
 
-function validateInputs(){
-const username = document.forms["FeedbackForm"]["username"];
-const email = document.forms["FeedbackForm"]["email"];
-const feedback = document.forms["FeedbackForm"]["feedback"];
+// function validateInputs(){
+// const username = document.forms["FeedbackForm"]["username"];
+// const email = document.forms["FeedbackForm"]["email"];
+// const feedback = document.forms["FeedbackForm"]["feedback"];
 
-var alphabets = /^[a-z]+$/i;
-iif(!(username.match(alphabets))){
-    document.getElementById("error").innerHTML = "*Please use alphabets (A-Z/a-z) only ";
+// var alphabets = /^[a-z]+$/i;
+// iif(!(username.match(alphabets))){
+//     document.getElementById("error").innerHTML = "*Please use alphabets (A-Z/a-z) only ";
    
-}
-}
+// }
+// }
