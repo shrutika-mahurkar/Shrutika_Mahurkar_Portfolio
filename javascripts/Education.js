0d008ce31f25bb9608ed68ca68c3d7ea043eceb4
shrutika-mahurkar <shrutika.vmahurkar@gmail.com>
2021-12-12 13:16:24 -0800

    Education page edited

---
+++ javascripts/Education.js
@@ -0,0 +1,42 @@
+var app = new Vue({
+    el:"#app",
+    data:{
+Education:[
+    {
+        "school": "Boston University",
+        "major":"Software Development",
+        "type":"Masters of Science",
+        "yearconferred":2023,
+        "courses":["MET CS520","MET CS 601"]
+    },
+    {
+        
+        "school": "UC Santa Cruz",
+        "major": "Computer Programming",
+        "type": "Certificate",
+        "yearconferred":2021,
+        "courses":["MET CS520","MET CS 601"]
+    
+    },
+    {
+        
+        "school": "Nagpur University",
+        "major": "Electronics and Telecommnunication",
+        "type": "Bachelors of Engineering",
+        "yearconferred":2017,
+        "courses":["MET CS520","MET CS 601"]
+    
+    },
+    {
+        
+        "school": "Maharashtra Board",
+        "major": "Computer Science",
+        "type": "Diploma",
+        "yearconferred":2013,
+        "courses":["MET CS520","MET CS 601"]
+    }
+
+            ]
+        },
+
+})
